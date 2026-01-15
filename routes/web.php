ed29ec8acc31c60c64ab1d15623281558c146d53
Ryuuji13 <thegolden154@gmail.com>
2026-01-15 07:59:13 +0700

    jobsheet 2.1

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return ('Selamat Datang di Praktik Laravel 12!');
+});
+
+Route::get('/user/{id}', function ($id) {
+    return ('Profile User dengan ID: ' . $id);
+});
+
+Route::get('/kategori/{slug?}', function ($slug = 'umum') {
+    return ('Menampilkan Kategori: ' . $slug);
+});
+
+Route::get('/dashboard/statistik/pengunjung', function () {
+    return 'Halaman Statistik Pengunjung';
+})->name('admin.statistik');
+
+Route::redirect('/tentang-kami', '/about');
+
+Route::get('/about', function () {
+    return 'Ini adalah Halaman Tentang Kami yang baru';
+});
