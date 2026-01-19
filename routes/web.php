b9b2305652c86177c87b41ad31bfdbb45f9971b3
Ryuuji13 <thegolden154@gmail.com>
2026-01-19 11:31:06 +0700

    Jobsheet 2.3

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SekolahController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -33,3 +34,5 @@ Route::get('/about', function () {
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
 Route::get('/about-me', [PostController::class, 'about'])->name('posts.about');
+
+Route::get('/profil/{id}', [SekolahController::class, 'show']);
